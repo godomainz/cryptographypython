cc2eeceb079cd2ff7d49c409ae3d309ab3bdf659
Akila Mendis <akila@oneit.com.au>
2021-05-24 20:42:10 +0530

    012 Check the performance and understand how fast the space of permutations grows

--- Permutations.py
+++ Permutations.py
@@ -1,8 +1,16 @@
-from itertools import permutations
+import cProfile
 
-mylist = [1, 2, 3, 4, 5, 6, 7]
-list_of_permutations = permutations(mylist)
-cnt = 0
-for permutation in list_of_permutations:
-    cnt += 1
-print(len(mylist), cnt)
+def faculty(n):
+    if n <= 1:
+        return n
+    else:
+        return faculty(n - 1) * n
+
+
+def counter(n):
+    cnt = 0
+    for i in range(n):
+        cnt += 1
+    return cnt
+
+cProfile.run("counter(faculty(11))")
