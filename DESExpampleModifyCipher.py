07f9840dfd0c171272d6cb04b60fbf0b2680d37a
Akila Mendis <akila@oneit.com.au>
2021-06-05 22:36:50 +0530

    044 DES and modifying the cipher texts

---
+++ DESExpampleModifyCipher.py
@@ -0,0 +1,27 @@
+from pyDes import *
+
+def modify(cipher):
+    mod = [0]*len(cipher)
+    mod[9] = 1
+    # mod[10] = ord(' ') ^ ord('1')
+    # mod[11] = ord(' ') ^ ord('0')
+    # mod[11] = ord('1') ^ ord('0')
+    return bytes(mod[i] ^ cipher[i] for i in range(len(cipher)))
+
+message = "Give Bob:    10$ and send them to him"
+key = "DESCRYPT"
+iv = bytes([0]*8)
+k = des(key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+
+# Alice sending encrypted cipher
+cipher = k.encrypt(message)
+print("Length of plain text:", len(message))
+print("Length of cipher text:", len(cipher))
+print("Encrypted: ", cipher)
+
+# Bob modifying the cipher text
+cipher = modify(cipher)
+
+# This is the bank decrypting the message
+message = k.decrypt(cipher)
+print("Decrypted : ", message)
