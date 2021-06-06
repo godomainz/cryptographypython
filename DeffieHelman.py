8945df746761d4a31bc4e25b3261b694690debcc
Akila Mendis <akila@oneit.com.au>
2021-06-06 14:12:10 +0530

    053 Implementing a Group Generator

--- DeffieHelman.py
+++ DeffieHelman.py
@@ -11,10 +11,26 @@ def is_prime(p):
     else:
         return False
 
+
 def get_prime(size):
     while True:
         p = random.randrange(size, 2*size)
         if is_prime(p):
             return p
 
-print(get_prime(1000))
+
+def is_generator(g, p):
+    for i in range(1, p-1):
+        if (g**i) % p == 1:
+            return False
+    return True
+
+
+def get_generator(p):
+    for g in range(2, p):
+        if is_generator(g, p):
+            return g
+
+p = get_prime(1000)
+g = get_generator(p)
+print(g, p)
