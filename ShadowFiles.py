19e46b3d8a05bd47981653e99e3b1841a9d00f0a
Akila Mendis <akila@oneit.com.au>
2021-06-08 15:56:15 +0530

    067 Why use Salt and shown by implementation in Python

--- ShadowFiles.py
+++ ShadowFiles.py
@@ -3,8 +3,16 @@ import base64
 
 iterations = 45454
 salt = base64.b64decode("6VuJKkHVTdDelbNMPBxzw7INW2NkYlR/LoW4OL7kVAI=".encode())
-# SALTED-SHA512-PBKDF2
+validation = "SALTED-SHA512-PBKDF2"
 
 password = "password".encode()
 value = hashlib.pbkdf2_hmac("sha512", password, salt, iterations, dklen=128)
-print(base64.b64encode(value))
+entropy = base64.b64encode(value)
+print(entropy)
+print("Alice", validation, iterations, entropy)
+
+salt = "666".encode()
+password = "password".encode()
+value = hashlib.pbkdf2_hmac("sha512", password, salt, iterations, dklen=128)
+entropy = base64.b64encode(value)
+print("Bob", validation, iterations, entropy)
